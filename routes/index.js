cd8caabe6cc670c18fc1cf75deccc35d7d6d185f
Stephen Burke <burke.stephenpaul@gmail.com>
2020-10-13 12:37:09 -0500

    Added personal greating, name doesn't show

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DisneyLand' });
+  res.render('index', { title: 'Bearcats' });
 });
+
+router.get('/', (res) => {
+    res.render('index', { dev: 'Stephen Burke' });
+  });
 
 module.exports = router;
