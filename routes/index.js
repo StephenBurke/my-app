3295e65d07c3e8c653130c3aff0f639cb68afc5c
Stephen Burke <burke.stephenpaul@gmail.com>
2020-10-13 15:34:25 -0500

    first phase of varibles

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bearcats' });
 });
 
-var dev = {
-  firstName: "Stephen",
-  lastName: "Burke",
-  fullName: function(){
-    return this.firstName + " " + this.lastName;
-  }
-}
-
 module.exports = router;
