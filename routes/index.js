2c8e3e56e90591b783ef3e6583a972ff4c8591a3
Stephen Burke <burke.stephenpaul@gmail.com>
2020-10-13 12:49:57 -0500

    Added developer name

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bearcats' });
 });
 
-router.get('/', (res) => {
-    res.render('index', { dev: 'Stephen Burke' });
-  });
 
 module.exports = router;
