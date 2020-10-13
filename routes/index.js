6f84513e6143ab64872d36232473e2021d2af3f0
Stephen Burke <burke.stephenpaul@gmail.com>
2020-10-13 15:17:03 -0500

    Added unique message

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bearcats' });
 });
 
+var dev = {
+  firstName: "Stephen",
+  lastName: "Burke",
+  fullName: function(){
+    return this.firstName + " " + this.lastName;
+  }
+}
 
 module.exports = router;
